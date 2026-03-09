90bc3c96580675efe51ff399043c0d3d4936ccb5
inagege <ina.gege@web.de>
2026-03-09 08:28:36 +0100

    remove jupyter notebooks, instead add python only file

--- analysis/data_preparation_util.py
+++ analysis/data_preparation_util.py
@@ -1,13 +1,15 @@
-from typing import List
-
 import pandas as pd
 import os
+from typing import List
+import itertools
 
 def calculate_cm_metrics(split_data: dict[str, pd.DataFrame], classes: List[str]) -> pd.DataFrame:
     """
     Calculate confusion matrix metrics (TP, FP, FN, TN) for each class in each data split.
     """
     metrics = pd.DataFrame(columns=['split', 'class', 'TP', 'FP', 'FN', 'TN'])
+    #convert classes to string to avoid issues with NaN values
+    classes = [str(cl) for cl in classes]
 
     for split, data in split_data.items():
         for cl in classes:
@@ -21,8 +23,7 @@ def calculate_cm_metrics(split_data: dict[str, pd.DataFrame], classes: List[str]
 
 
 def split_data_by_modification(predictions: pd.DataFrame) -> dict[str, pd.DataFrame]:
-    splits = predictions['split'].unique()
-    splits.sort()
+    splits = sorted(predictions['split'].dropna().unique().tolist())
     split_data = {}
 
     for split in splits:
@@ -35,7 +36,7 @@ def prepare_data_for_analysis(modalities: List[str], noise: List[str], task: str
     """
     Prepare data for analysis by combining modified and unmodified datasets, calculating metrics, and saving the results.
     """
-    pred_folder = os.path.join("..", "out", task)
+    pred_folder = os.path.join("out", task)
     mod_str = "".join(sorted(modalities))
     noise_str = "".join(sorted(noise)) if noise else ""
 
@@ -44,12 +45,14 @@ def prepare_data_for_analysis(modalities: List[str], noise: List[str], task: str
     try:
         all_unmodified = pd.read_csv(os.path.join(pred_folder, f"prediction_{mod_str}_noise_.csv"))
         all_unmodified.replace('unmodified', f"unmodified/{mod_str}", inplace=True)
+        all_unmodified.replace('all', f"all/{mod_str}", inplace=True)
 
         predictions = pd.read_csv(os.path.join(pred_folder, f"prediction_{mod_str}_noise_{noise_str}.csv"))
 
         if mod_str != noise_str:
             only_unmodified = pd.read_csv(os.path.join(pred_folder, f"prediction_{no_noise_str}_noise_.csv"))
             only_unmodified.replace('unmodified', f"unmodified/{no_noise_str}", inplace=True)
+            only_unmodified.replace('all', f"all/{no_noise_str}", inplace=True)
         else:
             only_unmodified = pd.DataFrame()
 
@@ -61,11 +64,33 @@ def prepare_data_for_analysis(modalities: List[str], noise: List[str], task: str
         predictions = pd.concat([predictions, only_unmodified, all_unmodified], ignore_index=True)
 
     predictions = predictions[predictions['label']!='unknown']
+    predictions.dropna(inplace=True)
 
     split_data = split_data_by_modification(predictions)
+    #convert all columns to string to avoid issues with NaN values
+    split_data = {split: data.astype(str) for split, data in split_data.items()}
 
     metrics = calculate_cm_metrics(split_data, classes=predictions['label'].unique())
 
-    os.makedirs(os.path.join("out", task, "prepared_data"), exist_ok=True)
-    out_path = os.path.join("out", task, "prepared_data", f"prepared_{mod_str}_noise_{noise_str}.csv")
+    os.makedirs(os.path.join("analysis", "out", task, "prepared_data"), exist_ok=True)
+    out_path = os.path.join("analysis", "out", task, "prepared_data", f"prepared_{mod_str}_noise_{noise_str}.csv")
     metrics.to_csv(out_path, index=False)
+
+def create_subset(l: List[str]) -> List[List[str]]:
+    subsets = [list(s) for r in range(len(l) + 1) for s in itertools.combinations(l, r)]
+    return subsets
+
+
+if __name__ == "__main__":
+
+    all_modalities = ['i', 't']
+    all_noise = ['i', 't']
+
+    subsets_modalities = create_subset(l=all_modalities)
+    subsets_modalities.remove([])
+    subsets_noise = create_subset(l=all_noise)
+
+    for modalities in subsets_modalities:
+        for noise in subsets_noise:
+
+            prepare_data_for_analysis(modalities=modalities, noise=noise, task='test/IMDB')
--- analysis/plotting_util.py
+++ analysis/plotting_util.py
@@ -4,21 +4,34 @@ from typing import List
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import argparse
 
 from enum import Enum
 import re
 from matplotlib.lines import Line2D
 
-COLORS_MODALITIES = sns.color_palette('bright', 7)
+DATASET_MODALITIES = {
+    "meld": {"t", "a", "v"},
+    "homeprice": {"t", "i"},
+    "IMDB": {"t", "i"},
+    "voxceleb": {"a", "v"},
+    "nejm": {"t", "i"},
+    "marine": {"a", "i"},
+}
+
+COLORS_MODALITIES = sns.color_palette('bright', 10)
 
 class ModalityPlotting(Enum):
     AUDIO = ("Audio", COLORS_MODALITIES[0])
     VIDEO = ("Video", COLORS_MODALITIES[1])
     TEXT  = ("Text", COLORS_MODALITIES[2])
-    AUDIO_VIDEO = ("Audio, Video", COLORS_MODALITIES[3])
-    AUDIO_TEXT = ("Audio, Text", COLORS_MODALITIES[4])
-    VIDEO_TEXT = ("Video, Text", COLORS_MODALITIES[5])
-    ALL = ("Audio, Video, Text", COLORS_MODALITIES[6])
+    IMAGE = ("Image", COLORS_MODALITIES[3])
+    AUDIO_VIDEO = ("Audio, Video", COLORS_MODALITIES[4])
+    AUDIO_TEXT = ("Audio, Text", COLORS_MODALITIES[5])
+    AUDIO_IMAGE = ("Audio, Image", COLORS_MODALITIES[6])
+    IMAGE_TEXT = ("Image, Text", COLORS_MODALITIES[7])
+    VIDEO_TEXT = ("Video, Text", COLORS_MODALITIES[8])
+    ALL = ("Audio, Video, Text", COLORS_MODALITIES[9])
     UNKNOWN = ("Unknown", "#000000")
 
     def __init__(self, label, color):
@@ -29,32 +42,37 @@ class ModalityPlotting(Enum):
     def from_string(s: str):
         s = s.lower()
 
-        has_audio = bool(re.search(r"audio", s)) or bool(re.search(r"a", s))
-        has_video = bool(re.search(r"video", s)) or bool(re.search(r"v", s))
-        has_text  = bool(re.search(r"text",  s)) or bool(re.search(r"t", s))
+        has_audio = bool(re.search(r"a", s))
+        has_video = bool(re.search(r"v", s))
+        has_text  = bool(re.search(r"t", s))
+        has_image = bool(re.search(r"i", s))
 
-        key = (has_audio, has_video, has_text)
+
+        key = (has_audio, has_video, has_text, has_image)
 
         mapping = {
-            (True,  False, False): ModalityPlotting.AUDIO,
-            (False, True,  False): ModalityPlotting.VIDEO,
-            (False, False, True):  ModalityPlotting.TEXT,
-            (True,  True,  False): ModalityPlotting.AUDIO_VIDEO,
-            (True,  False, True):  ModalityPlotting.AUDIO_TEXT,
-            (False, True,  True):  ModalityPlotting.VIDEO_TEXT,
-            (True,  True,  True):  ModalityPlotting.ALL,
+            (True,  False, False, False): ModalityPlotting.AUDIO,
+            (False, True,  False, False): ModalityPlotting.VIDEO,
+            (False, False, True, False):  ModalityPlotting.TEXT,
+            (True,  True,  False, False): ModalityPlotting.AUDIO_VIDEO,
+            (True,  False, True, False):  ModalityPlotting.AUDIO_TEXT,
+            (False, True,  True, False):  ModalityPlotting.VIDEO_TEXT,
+            (True,  True,  True, False):  ModalityPlotting.ALL,
+            (False, False, False, True): ModalityPlotting.IMAGE,
+            (True,  False, False, True):  ModalityPlotting.AUDIO_IMAGE,
+            (False, False, True, True):  ModalityPlotting.IMAGE_TEXT
         }
 
         return mapping.get(key, ModalityPlotting.UNKNOWN)
 
 
 
-def extract_classes(data: pd.DataFrame) -> dict[str, pd.DataFrame]:
+def extract_classes(data: pd.DataFrame) -> dict[object, pd.DataFrame]:
     """
     Extract data for each class from the metrics DataFrame.
     """
-    classes = data['class'].unique()
-    classes.sort()
+    classes = [cl for cl in data['class'].unique().tolist() if pd.notna(cl)]
+    classes = sorted(classes)
     class_data = {}
 
     for cl in classes:
@@ -129,7 +147,7 @@ def plot_metric_per_class(modalities: List[str], noise: List[str], metric: str, 
     noise_str = "".join(sorted(noise)) if noise else ""
 
     try:
-        prepared_data = pd.read_csv(os.path.join("out", task, "prepared_data", f"prepared_{mod_str}_noise_{noise_str}.csv"))
+        prepared_data = pd.read_csv(os.path.join("analysis", "out", task, "prepared_data", f"prepared_{mod_str}_noise_{noise_str}.csv"))
     except FileNotFoundError as e:
         print(f"Prepared data file not found: {e}")
         return
@@ -154,11 +172,14 @@ def plot_metric_per_class(modalities: List[str], noise: List[str], metric: str, 
         split = []
         for ind, row in data.iterrows():
             score = calculate_score(metric, tp=row['TP'], fp=row['FP'], tn=row['TN'], fn=row['FN'])
-            if 'unmodified' not in row['split']:
+            if ('unmodified' not in row['split']) and ('all/' not in row['split']):
                 metrics.append(score)
                 split.append(row['split'])
             else:
-                mod = ModalityPlotting.from_string(row['split'])
+                if 'all/' in row['split']:
+                    mod = ModalityPlotting.from_string(row['split'].split('all/')[1])
+                else:
+                    mod = ModalityPlotting.from_string(row['split'])
                 axs[i].axhline(y=score, color=mod.color, linestyle='--')
                 if mod.label not in line_labels:
                     # create a legend handle for this unmodified split (one per unique split)
@@ -177,7 +198,30 @@ def plot_metric_per_class(modalities: List[str], noise: List[str], metric: str, 
         fig.subplots_adjust(right=0.85)  # make room on the right for the legend
         fig.legend(handles=line_handles, labels=line_labels, loc='upper right', bbox_to_anchor=(0.98, 0.95), title="Unmodified Input")
 
-    out_path = os.path.join("out", task, metric, f"{metric}_{mod_str}_noise_{noise_str}")
-    os.makedirs(os.path.join("out", task, metric), exist_ok=True)
+    out_path = os.path.join("analysis", "out", task, metric, f"{metric}_{mod_str}_noise_{noise_str}")
+    os.makedirs(os.path.join("analysis", "out", task, metric), exist_ok=True)
     plt.savefig(os.path.join(f"{out_path}.svg"), bbox_inches='tight')
-    plt.show()
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset", type=str, required=True, help="Dataset name (e.g., 'IMDB')")
+    parser.add_argument("--scored", type=bool, default=False, help="Whether scored or unscored")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    metrics = ['mcc', 'f1', 'recall', 'precision']
+    dataset = args.dataset
+    if '/' in dataset:
+        dataset = dataset.split('/')[1]
+    modalities = list(DATASET_MODALITIES[dataset])
+    if args.scored:
+        dataset = f"test/{dataset}"
+
+    for metric in metrics:
+        plot_metric_per_class(metric=metric, modalities=modalities, noise=[], task=dataset)
+        for mod in modalities:
+            plot_metric_per_class(metric=metric, modalities=modalities, noise=[mod], task=dataset)
+            plot_metric_per_class(metric=metric, modalities=[mod], noise=[], task=dataset)
+
